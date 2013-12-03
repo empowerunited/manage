62d240b2e69ed873f7d45287009de8da5e6d535b
meddle <n.tzvetinov@gmail.com>
2013-12-03 17:37:18 +0200

    Customizable attributes added to the project

--- app/controllers/manage/resource_controller.rb
+++ app/controllers/manage/resource_controller.rb
@@ -12,6 +12,8 @@ class Manage::ResourceController < Manage::ApplicationController
 
   respond_to :html
 
+  helper_method :list_index_fields, :list_edit_fields
+
   protected
   def collection
     assocation = end_of_association_chain.page(params[:page] || 1)
@@ -20,6 +22,43 @@ class Manage::ResourceController < Manage::ApplicationController
 
   def permitted_params
     params.permit!
+  end
+
+  def self.setup_fields(key, *fields)
+    # :all means all the fields - default
+    if fields.first.to_sym == :all
+      options = fields.extract_options!
+      options[:except] ||= []
+      options[:include] ||= []
+
+      self.resources_configuration[:self][key] = (resource_class.attribute_names + Array(options[:include])) - Array(options[:except]).map(&:to_s) 
+    else
+      self.resources_configuration[:self][key] = fields
+    end
+  end
+
+  def self.index_fields(*fields)
+    setup_fields(:index_fields, *fields)
+  end
+
+  def self.edit_fields(*fields)
+    setup_fields(:edit_fields, *fields)
+  end
+
+  def list_fields(key)
+    if self.resources_configuration[:self][key].blank?
+      resource_class.attribute_names - %w(id created_at updated_at)
+    else
+      self.resources_configuration[:self][key]
+    end
+  end
+
+  def list_index_fields
+    list_fields(:index_fields)
+  end
+
+  def list_edit_fields
+    list_fields(:edit_fields)
   end
 
 end
--- app/helpers/manage/resource_helper.rb
+++ app/helpers/manage/resource_helper.rb
@@ -1,7 +1,68 @@
 module Manage
   module ResourceHelper
+    def index_attributes
+      list_index_fields
+    end
+
+    def edit_attributes
+      list_edit_fields
+    end
+
     def attributes
       resource_class.attribute_names - %w(id created_at updated_at)
     end
+
+    def attribute_value(scope, attr)
+      prefix_attr, rest_attr, custom_query, custom_format = _parse_attribute(attr)
+      value = scope.public_send(prefix_attr)
+
+      if value.is_a?(ActiveRecord::Associations::CollectionProxy )
+        if rest_attr.empty?
+          value = custom_query.present? ? custom_query.call(value) : value
+
+          value.map do |entity|
+            ("<a href=\"#{prefix_attr}/#{entity.id}\">#{custom_format.present? ? custom_format.call(entity) : entity.id }</a>")
+          end.join(', ').html_safe
+        else
+          value.map do |entity|
+            attribute_value(entity, rest_attr.join('.'))
+          end.join(', ')
+        end
+      else
+        rest_attr.empty? ? value.to_s : attribute_value(value, rest_attr.join('.'))
+      end
+
+    end
+
+    def attribute_header(resource_class, attr, prefix='')
+      prefix_attr, rest_attr = _parse_attribute(attr)
+      header = resource_class.human_attribute_name(prefix_attr)
+
+      rest_attr.empty? ? prefix + header : attribute_header(resource_class, rest_attr.join('.'), prefix + header + ' ')
+    end
+
+    private
+    def _parse_attribute(attr)
+      custom_query = nil
+      custom_format = nil
+
+      if attr.is_a?(Hash)
+        prefix_attr = attr.keys.first
+        rest_attr = attr[prefix_attr]
+
+        if rest_attr.is_a?(String)
+          rest_attr = Array(rest_attr)
+        else
+          custom_query = attr[prefix_attr][:query]
+          custom_format = attr[prefix_attr][:format]
+          rest_attr = []
+        end
+      else
+        prefix_attr, *rest_attr = attr.to_s.split('.')
+      end
+
+      return prefix_attr, rest_attr, custom_query, custom_format
+    end
+
   end
 end
