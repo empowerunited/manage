c451fcb0e860287e18901988d275ad14caaeb7dd
Aleksandar Dimov <aleksandar@empowerunited.com>
2014-10-08 14:25:47 +0300

    Delete Yatoto constant.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Yatoto::Application.config.session_store :cookie_store, key: '_YatotoManage_session'
+#Yatoto::Application.config.session_store :cookie_store, key: '_YatotoManage_session'
